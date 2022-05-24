073d04f4d93628c2a70d31630dae06c7d268eb1e
Diego Barneche <barnechedr@gmail.com>
2022-05-24 20:50:21 +0800

    fixed example

--- example.R
+++ example.R
@@ -3,9 +3,9 @@ tar_dir({
   # Parameterized R Markdown:
   dir.create("doc")
   report <- readLines("https://raw.githubusercontent.com/dbarneche/tar_example_files/main/doc/report.Rmd")
-  writeLines(lines, "doc/report.Rmd")
+  writeLines(report, "doc/report.Rmd")
   latex <- readLines("https://raw.githubusercontent.com/dbarneche/tar_example_files/main/doc/template.latex")
-  writeLines(lines, "doc/template.latex")
+  writeLines(latex, "doc/template.latex")
   tar_script({
     options(tidyverse.quiet = TRUE)
     library(tidyverse)
@@ -17,13 +17,14 @@ tar_dir({
       tar_target(my_figure, {
         (ggplot(data.frame(x = 1, y = 1)) +
           geom_point(aes(x, y))) %>%
-          ggsave(filename = "my_fig.png", plot = ., device = "png")
+          ggsave(filename = "output/figures/my_fig.png", plot = ., 
+                 device = "png")
       }),
       tar_target(latex_file,
         "doc/template.latex", format = "file"
       ),
       tar_render(raw_pdf,
-        "report.Rmd", output_dir = "output/doc/",
+        "doc/report.Rmd", output_dir = "output/doc/",
         intermediates_dir = "output/doc/", clean = FALSE
       )
     )
